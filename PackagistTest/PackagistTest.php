fd90445f3405f5947fd123b856f8665fa5f3ebb8
d9magai <kumagai@sencorp.co.jp>
2014-08-14 02:34:37 -0700

    PackagistTest.php

---
+++ PackagistTest/PackagistTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace PackagistTest;
+
+class PackagistTest
+{
+    public function __construct($path = './hello.log')
+    {
+    }
+
+    public function hello($name = 'World')
+    {
+        echo 'hellow world';
+    }
+}
+
